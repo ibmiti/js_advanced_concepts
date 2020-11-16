e3da9c01ac92eeaa50fb80d8860a938f025a3193
Dimitri M <ibmiti@gmail.com>
2020-11-16 13:58:10 -0500

    sending

--- execution_context.js
+++ execution_context.js
@@ -1,12 +1,14 @@
 function global_context(bool,bool2){
-    console.log(`it is ${bool} function is apart of the global context, or scope`);
+    console.log(`it is ${bool} this function is apart of the global context, or scope`);
+    let a = ' this is also function context and not global';
+    console.log(a);
     function functional_context(bool2){
-        console.log(`it is ${bool2} is the functional context $`);
+        console.log(`it is ${bool2} this function is functional context`);
     }
     return functional_context(bool2);
 }
-
 window.global_context(true, true);
+
 
 
 
